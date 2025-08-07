5e6ed93794e7b8c4e8c7af44b1f28bb39c6b6125
Chala Temesgen <ctemesgen85@gmail.com>
2025-08-07 08:30:53 -0300

    Home Page added

---
+++ vote-app/app/page.tsx
@@ -0,0 +1,9 @@
+import VoteCard from '@/components/VoteCard'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <VoteCard />
+    </main>
+  )
+}
