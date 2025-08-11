8c19a26f0b8d6fc15a972557764e30fe89f0850a
Chala Temesgen <ctemesgen85@gmail.com>
2025-08-11 05:42:47 -0300

    next configured

--- vote-app/next.config.ts
+++ vote-app/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_ADMIN_SECRET: process.env.NEXT_PUBLIC_ADMIN_SECRET,
+  },
 };
 
 export default nextConfig;
